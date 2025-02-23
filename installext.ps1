8079d8e9803c298c15e26cc8d1760b1fe0cb8a32
timfurnival-ia <timf@insightfulazure.co.uk>
2025-02-23 23:02:02 +0000

    Create installext.ps1

---
+++ installext.ps1
@@ -0,0 +1,14 @@
+
+        # Install VSCode extensions
+        code --install-extension ms-python.python;
+        code --install-extension ms-vscode-remote.remote-wsl;
+        code --install-extension d-biehl.robotcode
+        code --install-extension ms-azuretools.vscode-bicep;
+        code --install-extension ms-azuretools.vscode-docker
+        code --install-extension hashicorp.terraform
+        code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+        code --install-extension ms-vscode.powershell
+        code --install-extension SonarSource.sonarlint-vscode
+        code --install-extension redhat.vscode-yaml
+        code --install-extension ms-vscode.vscode-node-azure-pack
+        code --install-extension prompt-flow.prompt-flow
