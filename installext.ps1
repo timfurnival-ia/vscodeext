557e7be86ba34482563ec4a2ab0125b36ebdd6b5
timfurnival-ia <timf@insightfulazure.co.uk>
2025-02-23 23:18:37 +0000

    Update installext.ps1

--- installext.ps1
+++ installext.ps1
@@ -1,5 +1,8 @@
 
         # Install VSCode extensions
-        code --install-extension ms-python.python --force;
-        code --install-extension ms-vscode-remote.remote-wsl;
-        code --install-extension d-biehl.robotcode
+        code --install-extension ms-vscode.azure-repos-0.40.0.vsix --force
+        code --install-extension ms-toolsai.vscode-ai-1.2.0.vsix --force
+        code --install-extension ms-python.python-2025.0.0-win32-x64.vsix --force
+      
+      
+      
