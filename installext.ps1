4fb9b4e6d7980e0815c57f02b8d792ec6a381a01
timfurnival-ia <timf@insightfulazure.co.uk>
2025-02-24 00:04:41 +0000

    Update installext.ps1

--- installext.ps1
+++ installext.ps1
@@ -1,8 +1,4 @@
-
         # Install VSCode extensions
-        code --install-extension ms-vscode.azure-repos-0.40.0.vsix --force
-        code --install-extension ms-toolsai.vscode-ai-1.2.0.vsix --force
-        code --install-extension ms-python.python-2025.0.0-win32-x64.vsix --force
-      
-      
-      
+        code --install-extension ms-vscode.azure-repos-0.40.0.vsix --force --extensions-dir="C:\Program Files\Microsoft VS Code\resources\app\extensions"
+        code --install-extension ms-toolsai.vscode-ai-1.2.0.vsix --force --extensions-dir="C:\Program Files\Microsoft VS Code\resources\app\extensions"
+        code --install-extension ms-python.python-2025.0.0-win32-x64.vsix --force  --extensions-dir="C:\Program Files\Microsoft VS Code\resources\app\extensions"
